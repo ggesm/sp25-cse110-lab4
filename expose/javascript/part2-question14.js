ce2a49fcfb3f1e14262d6847f6ce42503d6bbc8a
ggesm <giana.gesmundo@gmail.com>
2025-04-30 00:16:07 -0700

    q4

---
+++ expose/javascript/part2-question14.js
@@ -0,0 +1,19 @@
+// Question 14: Comparison
+
+// A
+console.log('2' > 1);
+
+// B
+console.log('2' < '12');
+
+// C
+console.log(2 == '2');
+
+// D
+console.log(2 === '2');
+
+// E
+console.log(true == 2);
+
+// F
+console.log(true === Boolean(2));

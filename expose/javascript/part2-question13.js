24d7c0961cce673979f3649023984c482f986484
ggesm <giana.gesmundo@gmail.com>
2025-04-30 00:11:37 -0700

    q3

---
+++ expose/javascript/part2-question13.js
@@ -0,0 +1,24 @@
+// Question 13: Arithmetic
+// A
+console.log('3' + 2);
+
+//B
+console.log('3'- 2);
+
+//C
+console.log(3 + null);
+
+//D
+console.log('3' + null);
+
+//E
+console.log(true + 3);
+
+//F
+console.log(false + null);
+
+//G
+console.log('3' + undefined);
+
+//H
+console.log('3'- undefined);
